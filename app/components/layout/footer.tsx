324d64a16e143888bd9d81c3b6fce21b6b4d7755
sponsorCoinAdmin <sponsorCoinAdmin@proton.me>
2023-11-16 16:36:01 -0500

    WIP

--- app/components/layout/footer.tsx
+++ app/components/layout/footer.tsx
@@ -1,8 +1,6 @@
 import React from "react";
 
-function Footer(props: { address: any; isConnected: any; connect: any; }) {
-  const {address, isConnected, connect} = props;
-
+function Footer() {
   return (
     <footer>
       <h1>Footer</h1>
---
+++ app/components/layout/title.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Title() {
+
+  return (
+    <titleName>
+      <h1>Title</h1>
+    </titleName>
+  );
+}
+
+export default Title;
