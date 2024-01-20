8341d1e2426f6ef4cb3ba639ba0a31001e3c2434
sponsorCoinAdmin <sponsorCoinAdmin@proton.me>
2024-01-20 05:01:18 -0500

    WIP

--- app/components/panes/header.tsx
+++ app/components/panes/header.tsx
@@ -1,26 +1,25 @@
 'use client'
 
-import React from "react";
+import React, { useState } from "react";
 import styles from "../../styles/Header.module.css"
 import spCoin_png from '../../resources/images/spCoin.png'
 import eth_png from '../../resources/images/eth.png'
 import Image from 'next/image'
 import Link from 'next/link'
 import ConnectButton from "./ConnectButton";
-import { useState } from "react";
-import { Network } from "ethers";
 
-const updateNetwork = (newNetwork:string) =>  {
-  const [netWork, setNetWork] = useState("ZZZZ");
-
-  if (newNetwork != null && newNetwork !== 'undefined') 
-    setNetWork(newNetwork)
-
-  return netWork;
-}
+import {
+  watchNetwork,
+} from "@wagmi/core";
 
 function Header() {
-  const [netWork, setNetWork] = useState("Ethereum");
+  const [networkData, setNetworkData]=useState({chainId:'1',name:'Ethereum'});    
+  const unwatchNetwork = watchNetwork((network) => processNetworkChange(network))
+  const processNetworkChange = ( network:any ) => {
+    setNetworkData({chainId:network.chain.id, name:network.chain.name})
+    console.debug( "HEADER NETWORK = " + JSON.stringify(network, null, 2) )
+    console.debug( "HEADER NETWORK NAME = " + network.chain.name )
+  }  
 
   return (
     <header>
@@ -29,13 +28,16 @@ function Header() {
         <div className={styles.headerItem}><Link href="/SponsorCoin">SponsorCoin</Link></div>
         <div className={styles.headerItem}><Link href="/Exchange">Exchange</Link></div>
         <div className={styles.headerItem}><Link href="/Admin">Admin</Link></div>
+        {/* <div className={styles.headerItem}><Link href="/0X">0X</Link></div> */}
       </div>
       <div className={styles.rightH}>
         <div className={styles.headerItem}>
           <Image src={eth_png} width={25} height={25} alt="Ethereum Logo" />
-          &nbsp;&nbsp; {netWork}
+          &nbsp;&nbsp;{networkData?.name}
         </div>
         <ConnectButton />
+
+        {/* {<ConnectButton />} */}
       </div>
     </header>
   );
