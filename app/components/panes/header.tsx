57d75a378897882089cc7f3fd7d8dc8f5fedc2a6
sponsorCoinAdmin <sponsorCoinAdmin@proton.me>
2024-01-24 22:00:49 -0500

    WIP

--- app/components/panes/header.tsx
+++ app/components/panes/header.tsx
@@ -16,10 +16,25 @@ const imgHome = "https://github.com/sponsorCoinAdmin/spCoinData/blob/main/resour
 const imgOptions = ".png?raw=true"
 
 function Header() {
+/*
+  const [networkData, setNetworkData]=useState({chainId:'1',name:'Ethereum'});    
+  const unwatchNetwork = watchNetwork((network) => processNetworkChange(network))
+  const processNetworkChange = ( network:any ) => {
+    setNetworkData({chainId:network.chain.id, name:network.chain.name})
+    console.debug( "HEADER NETWORK CHAIN ID = " + network.chain.id)
+    console.debug( "HEADER NETWORK NAME     = " + network.chain.name )
+  }
+*/
+
+  function getTokenImage(chainId:string) {
+    let imgURL:string = imgHome+chainId+imgOptions;
+    return imgURL
+  }
+
   return (
     <header>
       <div className={styles.leftH}>
-        <Image className={styles.avatarImg} src={spCoin_png} width={25} height={25} alt="Sponsor Coin Logo" />
+        <Image className={styles.imgOptions} src={spCoin_png} width={25} height={25} alt="Sponsor Coin Logo" />
         <div className={styles.headerItem}><Link href="/SponsorCoin">SponsorCoin</Link></div>
         <div className={styles.headerItem}><Link href="/Exchange">Exchange</Link></div>
         <div className={styles.headerItem}><Link href="/Admin">Admin</Link></div>
