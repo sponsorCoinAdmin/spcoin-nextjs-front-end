1ffa3fc1730422d8684ae610a694dd6b06a40f50
sponsorCoinAdmin <sponsorCoinAdmin@proton.me>
2024-01-30 14:54:43 -0500

    cleanup

--- app/api copy/mainnet/networkConfig.tsx
+++
@@ -1,6 +0,0 @@
-import { getURLParams, OX_API_KEY, FEE_WALLET_DETAILS } from '../apiConfig'
-const networkName="Ethereum"
-const networkURL = "https://api.0x.org";
-const chainId = 1
-
-export { getURLParams, OX_API_KEY, chainId, networkName, networkURL, FEE_WALLET_DETAILS }
