606a7525040d53e8a77ecc12accef012258ccf2d
sponsorCoinAdmin <sponsorCoinAdmin@proton.me>
2024-06-23 14:43:29 -0400

    WIP

--- lib/structure/types.ts
+++ lib/structure/types.ts
@@ -1,5 +1,21 @@
 import { Address } from "viem"
 
+enum  EXCHANGE_STATE { PRICE, QUOTE, APPROVE, PENDING }
+
+enum  DISPLAY_STATE {
+  OFF, SPONSOR_SELL_ON, SPONSOR_SELL_OFF, SPONSOR_BUY, RECIPIENT, CONFIG
+}
+
+enum  FEED_TYPE { TOKEN_LIST, AGENT_WALLETS, RECIPIENT_WALLETS }
+
+interface PriceRequestParams {
+  sellToken: Address|string;
+  buyToken: Address|string;
+  buyAmount?: string;
+  sellAmount?: string;
+  connectedWalletAddr?: string;
+}
+
 type TokenContract = {
   chainId : number | undefined,
   address : any,
@@ -20,6 +36,16 @@ type TokenContract = {
   totalSupply : any
   img: string | undefined;
 }
+
+type TokenContract = {
+  chainId: number;
+  address: Address|string;
+  name: string;
+  symbol: string;
+  decimals: number;
+  img: string;
+}
+
 */
 
 type ContractRecs = {
@@ -28,8 +54,55 @@ type ContractRecs = {
     decimalRec:any,
     totalSupplyRec:any
   }
+  
+type WalletElement = {
+  address: Address|string;
+  name: string;
+  symbol: string;
+  img: string;
+  url: string;
+}
 
-  export type{
-    TokenContract,
-    ContractRecs
-  }
+type TradeData = {
+  chainId: number;
+  networkName: string;
+  sellAmount:string;
+  buyAmount:string;
+  tradeDirection:string
+  displayState: DISPLAY_STATE;
+  state: EXCHANGE_STATE;
+  slippage: string;
+}
+
+type ExchangeContext = {
+  data: TradeData;
+  network: NetworkElement;
+  sellTokenContract: TokenContract;
+  buyTokenContract: TokenContract;
+  recipientWallet: WalletElement;
+  agentWallet: WalletElement;
+}
+
+type NetworkElement = {
+  chainId: number;
+  name: string;
+  symbol: string;
+  img: string;
+  url: string;
+}
+
+export {
+  EXCHANGE_STATE,
+  DISPLAY_STATE,
+  FEED_TYPE
+}
+
+export type {
+  ContractRecs,
+  ExchangeContext,
+  NetworkElement,
+  PriceRequestParams,
+  TokenContract,
+  TradeData,
+  WalletElement
+}
