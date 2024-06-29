aab42ae23fa2dcd4a67a48cd1f766904e955cf6b
sponsorCoinAdmin <sponsorCoinAdmin@proton.me>
2024-06-29 14:38:49 -0400

    WIP

--- app/(menu)/layout.tsx
+++ app/(menu)/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
-import '../globals.css'
+import '@/app/globals.css'
 import Footer from '@/components/panes/footer'
 import Header from '@/components/panes/header'
 import SpCoinWrapper from '@/components/SpCoinWrapper'
