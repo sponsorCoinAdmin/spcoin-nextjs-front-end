424166c7fe58b1a42f5bda1ed97a2f5a99411f15
sponsorCoinAdmin <sponsorCoinAdmin@proton.me>
2024-07-07 13:41:18 -0400

    Add AddSponsorButton

--- components/containers/BuyContainer.tsx
+++ components/containers/BuyContainer.tsx
@@ -4,7 +4,7 @@ import AssetSelect from './AssetSelect';
 import { DISPLAY_STATE, TokenContract } from '@/lib/structure/types';
 import { getERC20WagmiClientBalanceOf } from '@/lib/wagmi/erc20WagmiClientRead';
 import AddSponsorButton from '../Buttons/AddSponsorButton';
-// import { isSpCoin } from '@/lib/spCoin/utils';
+import { isSpCoin } from '@/lib/spCoin/utils';
 
 type Props = {
   activeAccount: any,
@@ -18,7 +18,9 @@ type Props = {
 const BuyContainer = ({activeAccount, buyAmount, buyTokenContract, setBuyAmount, setDisplayState, disabled} : Props) => {
 
   try {
-  const balanceOf = (getERC20WagmiClientBalanceOf(activeAccount.address, buyTokenContract.address || "") || "0");
+    let IsSpCoin = isSpCoin(buyTokenContract);
+    console.debug("BuyContainer.isSpCoin = " + IsSpCoin)
+    const balanceOf = (getERC20WagmiClientBalanceOf(activeAccount.address, buyTokenContract.address || "") || "0");
   
     return (
       <div className={styles.inputs}>
@@ -27,15 +29,9 @@ const BuyContainer = ({activeAccount, buyAmount, buyTokenContract, setBuyAmount,
         <AssetSelect TokenContract={buyTokenContract} id={"buyTokenDialog"} disabled={disabled}></AssetSelect>
         <div className={styles["buySell"]}>You receive</div>
         <div className={styles["assetBalance"]}>Balance: {balanceOf}</div>
-        {/* <div id="addSponsorshipDiv" className={styles[`addSponsorshipDiv`]} onClick={() => setDisplayState(DISPLAY_STATE.RECIPIENT)}>
-          {true ? <div className={styles["centerContainer"]} >Add Sponsorship</div> : null}
-          <div className={styles["centerContainer"]} >Add Sponsorship</div>
-        </div> */}
-        <AddSponsorButton activeAccount={activeAccount} 
-                          buyAmount={buyAmount} buyTokenContract={buyTokenContract} 
-                          setBuyAmount={undefined} setDisplayState={function (displayState: DISPLAY_STATE): void {
-          throw new Error('Function not implemented.');
-        } } disabled={false} />
+        {IsSpCoin ?
+          <AddSponsorButton activeAccount={activeAccount} buyTokenContract={buyTokenContract} setDisplayState={setDisplayState} />
+          : null}
       </div>
     );
   } catch (err:any) {
