1303bd381fc066d14fb89ae832b638b5ec6f933e
sponsorCoinAdmin <sponsorCoinAdmin@proton.me>
2024-07-09 21:39:03 -0400

    WIP

--- components/Buttons/ManageSponsorsButton.tsx
+++ components/Buttons/ManageSponsorsButton.tsx
@@ -1,6 +1,8 @@
 import styles from '@/styles/Exchange.module.css';
 import { DISPLAY_STATE, TokenContract } from '@/lib/structure/types';
 import { getERC20WagmiClientBalanceOf } from '@/lib/wagmi/erc20WagmiClientRead';
+import { showElement } from '@/lib/spCoin/guiControl';
+import { openDialog } from '../Dialogs/Dialogs';
 
 type Props = {
   activeAccount: any,
@@ -13,12 +15,10 @@ const ManageSponsorsButton = ({activeAccount, buyTokenContract, setDisplayState}
   try {
   const balanceOf = (getERC20WagmiClientBalanceOf(activeAccount.address, buyTokenContract.address || "") || "0");
     return (
-      <>
-        <div id="manageSponsorshipsDiv" className={styles[`manageSponsorshipsDiv`]} onClick={() => setDisplayState(DISPLAY_STATE.RECIPIENT)}>
+        <div id="manageSponsorshipsDiv" className={styles[`manageSponsorshipsDiv`]} onClick={() => openDialog("#manageSponsorshipsDialog")}>
           <div className={styles["centerTop"]} >Manage</div>
           <div className={styles["centerBottom"]} >Sponsorships</div>
         </div>
-      </>
     );
   } catch (err:any) {
     console.debug (`Sell Container Error:\n ${err.message}`)
