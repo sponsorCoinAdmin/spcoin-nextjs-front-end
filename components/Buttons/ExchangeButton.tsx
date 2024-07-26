3f44c966dc889ed797778580b12abfd704072b0e
sponsorCoinAdmin <sponsorCoinAdmin@proton.me>
2024-07-26 15:59:01 -0400

    WIP

--- components/Buttons/ExchangeButton.tsx
+++ components/Buttons/ExchangeButton.tsx
@@ -2,7 +2,6 @@ import React from 'react'
 import styles from '@/styles/Exchange.module.css'
 import { ExchangeContext } from '@/lib/structure/types'
 
-
 type Props = {
   exchangeContext:ExchangeContext,
 }
@@ -13,15 +12,32 @@ const CustomConnectButton = ({ exchangeContext}:Props) => {
   }
 
   const noTradingAmounts = () => {
+    let noTradingAmount:boolean = false;
+    try {
     // let noTradingAmount:boolean = !( exchangeContext.tradeData.sellAmount === "0"  || exchangeContext.tradeData.buyAmount === "0" )
-    let noTradingAmount:boolean = ( exchangeContext.tradeData.sellAmount === "0" )
+    noTradingAmount = ( exchangeContext.tradeData.sellAmount === "0" )
     // console.log(`ExchangeButton => exchangeContext.tradeData.sellAmount = ${exchangeContext.tradeData.sellAmount}\noTradingAmount = ${noTradingAmount}`);
     // alert (validTradingAmount)
+    } catch(err:any) {
+      alert(`ERROR: CustomConnectButton.noTradingAmounts: ${err.message}`)
+    }
     return noTradingAmount;
   }
 
   const insufficientBalance = () => {
-    return ( exchangeContext.tradeData.sellBalanceOf <  exchangeContext.tradeData.sellAmount)
+    let insufficientBalance:boolean = false;
+     try {
+      console.debug(`CustomConnectButton.insufficientBalance: exchangeContext.tradeData.sellBalanceOf = "${exchangeContext.tradeData.sellBalanceOf}"\n
+        exchangeContext.tradeData.sellAmount = "${exchangeContext.tradeData.sellAmount}"`)
+      let bigIntSellBalanceOf:BigInt  = BigInt(exchangeContext.tradeData.sellBalanceOf);
+      let bigIntSellAmount:BigInt  = BigInt(exchangeContext.tradeData.sellAmount);
+      // alert(`SUCCESS: CustomConnectButton. insufficientBalance = ${insufficientBalance}`)
+
+      insufficientBalance = bigIntSellBalanceOf <  bigIntSellAmount
+    } catch(err:any) {
+      alert(`ERROR: CustomConnectButton.insufficientBalance: ${err.message}`)
+    }
+    return insufficientBalance;
   }
 
   return (
@@ -32,7 +48,12 @@ const CustomConnectButton = ({ exchangeContext}:Props) => {
         type="button"
         className={styles["exchangeButton"]}
         >
-        {noTradingAmounts() ? "Enter an Amount" : "Insufficient Balance"}
+        {/* {noTradingAmounts() ? "Enter an Amount" : "Insufficient Balance"} */}
+
+        {noTradingAmounts() ? "Enter an Amount" : 
+        insufficientBalance() ? "Insufficient Balance" :
+        "SWAP"}
+
       </button>
     </div>
   )
