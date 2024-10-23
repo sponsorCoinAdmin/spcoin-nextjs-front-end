d901b544a7ea8bc70e2a7804095a7817e9e24e39
sponsorCoinAdmin <sponsorCoinAdmin@proton.me>
2024-10-23 02:17:54 -0400

    WIP

--- components/containers/RecipientContainer.tsx
+++ components/containers/RecipientContainer.tsx
@@ -16,7 +16,7 @@ type Props = {
 
 const RecipientContainer = ({setRecipientCallBack} : Props) => {
   // const RecipientContainer = ({setRecipientCallBack} : Props) => {
-  // alert("RecipientContainer:\n" + JSON.stringify(recipientAccount,null,2))
+  alert("RecipientContainer:\n" + JSON.stringify(exchangeContext.recipientAccount,null,2))
   // let urlParms:string = `/Recipient?address=${recipientAccount?.address}`
   const [recipientAccount, setRecipientAccount] = useState<AccountRecord|undefined>(exchangeContext.recipientAccount);
 
