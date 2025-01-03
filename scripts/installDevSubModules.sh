271216e00861496320ba725635af8e3b949f3f56
sponsorCoinAdmin <sponsorCoinAdmin@proton.me>
2025-01-02 19:14:20 -0500

    WIP

---
+++ scripts/installDevSubModules.sh
@@ -0,0 +1,10 @@
+CURR_DIR=$(PWD)
+cd node_modules-dev/common/spcoin-lib
+npm i
+cd $CURR_DIR
+cd node_modules-dev/bg/spcoin-access-modules
+npm i
+cd $CURR_DIR
+cd node_modules-dev/bg/spcoin-with-modules
+npm i
+cd $CURR_DIR
