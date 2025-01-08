28326dab6cbe4db812d30e77a05af58ab48e976d
sponsorCoinAdmin <sponsorCoinAdmin@proton.me>
2025-01-08 01:35:10 -0500

    WIP

--- scripts/setupEnvAliases.sh
+++ scripts/setupEnvAliases.sh
@@ -34,6 +34,7 @@ createNewEnvironmentFile() {
 
 
     echo ". \$ACTIVE_ENV_PATH/.a"                                                        | tee -a $ACTIVE_ENV_FILE_PATH
+    echo "m"                                                                             | tee -a $ACTIVE_ENV_FILE_PATH
 }
 
 #SET UP BASH ENVIRONMENT
@@ -43,10 +44,9 @@ insertOnce "set -o vi" ~/.bashrc;
 echo "Adding sponsor coin startup configuration Files to bootstrap file ~/.bashrc"
 sed -i '/ACTIVE_ENV_FILE_PATH/d' ~/.bashrc
 sed -i '/ACTIVE_PROJECT_PATH/d' ~/.bashrc
+echo "export ACTIVE_ENV_FILE_PATH=$ACTIVE_ENV_FILE_PATH" | tee -a ~/.bashrc;
 echo ". \$ACTIVE_ENV_FILE_PATH"                          | tee -a ~/.bashrc;
 echo "cd \$ACTIVE_PROJECT_PATH"                          | tee -a ~/.bashrc;
 
-#RUN THE ENVIRONMENT SETUP
-#RUN THE ENVIRONMENT SETUP
 echo "EXECUTING: . $ACTIVE_ENV_FILE_PATH"
 . $ACTIVE_ENV_FILE_PATH
