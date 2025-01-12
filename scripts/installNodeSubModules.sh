877cceadf3bc9cec8da7a4cf2a97324ffd4c063f
sponsorCoinAdmin <sponsorCoinAdmin@proton.me>
2025-01-12 07:37:06 -0500

    WIP

--- scripts/installNodeSubModules.sh
+++ scripts/installNodeSubModules.sh
@@ -1,11 +1,16 @@
-CURR_DIR=$(PWD)
-echo START INSTALLING NPM Modules in $CURR_DIR/node_modules-dev/spcoin-back-end/spcoin-common/spcoin-lib
-cd $CURR_DIR/node_modules-dev/spcoin-back-end/spcoin-common/spcoin-lib
-npm i
-echo START INSTALLING NPM Modules in $CURR_DIR/node_modules-dev/spcoin-back-end/spcoin-access-modules
-cd $CURR_DIR/node_modules-dev/spcoin-back-end/spcoin-access-modules
-npm i
-echo START INSTALLING NPM Modules in $CURR_DIR/node_modules-dev/spcoin-back-end/spcoin-with-modules
-cd $CURR_DIR/node_modules-dev/spcoin-back-end/spcoin-with-modules-cjs
-npm i
-cd $CURR_DIR
+echo START INSTALLING NPM Modules in $PWD/node_modules-dev/spcoin-back-end
+cd ./node_modules-dev
+. ./scripts/installNodeSubModules.sh
+cd ..
+# echo START INSTALLING NPM Modules in $CURR_DIR/node_modules-dev/spcoin-back-end/spcoin-common/spcoin-lib
+# cd $CURR_DIR/node_modules-dev/spcoin-back-end/spcoin-common/spcoin-lib
+# npm i
+# cd ..
+# echo START INSTALLING NPM Modules in $CURR_DIR/node_modules-dev/spcoin-back-end/spcoin-access-modules
+# cd $CURR_DIR/node_modules-dev/spcoin-back-end/spcoin-access-modules
+# npm i
+# cd ..
+# echo START INSTALLING NPM Modules in $CURR_DIR/node_modules-dev/spcoin-back-end/spcoin-with-modules
+# cd $CURR_DIR/node_modules-dev/spcoin-back-end/spcoin-with-modules-cjs
+# npm i
+# cd ..
