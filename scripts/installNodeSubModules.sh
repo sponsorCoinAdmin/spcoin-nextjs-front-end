c33aade3648d666293353d5f4b0c5a97cf832592
sponsorCoinAdmin <sponsorCoinAdmin@proton.me>
2025-01-13 05:48:33 -0500

    WIP

--- scripts/installNodeSubModules.sh
+++ scripts/installNodeSubModules.sh
@@ -1,8 +1,13 @@
 echo "=======================spcoin-nextjs-front-end========================================="
 echo START INSTALLING NPM Modules in $PWD/node_modules-dev/spcoin-back-end
 cd ./node_modules-dev
-. ./scripts/installNodeSubModules.sh
+ . ./scripts/installNodeSubModules.sh
 cd ..
+
+
+
+
+
 # echo START INSTALLING NPM Modules in $CURR_DIR/node_modules-dev/spcoin-back-end/spcoin-common/spcoin-lib
 # cd $CURR_DIR/node_modules-dev/spcoin-back-end/spcoin-common/spcoin-lib
 # npm i
