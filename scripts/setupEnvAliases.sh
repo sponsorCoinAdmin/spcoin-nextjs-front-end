177b47b99487226d048901c13efdc3d3115c1739
sponsorCoinAdmin <sponsorCoinAdmin@proton.me>
2025-01-14 20:08:49 -0500

    WIP

--- scripts/setupEnvAliases.sh
+++ scripts/setupEnvAliases.sh
@@ -1,7 +1,7 @@
 echo ==== SETTING UP ENVIRONMENT VARIABLES ====
 export ACTIVE_PROJECT_NAME=$(basename $PWD)
 export ACTIVE_ROOT_DIR="$(dirname "$PWD")"
-export ACTIVE_PROJECT_PATH=$PWD
+export ACTIVE_PROJECT_HOME=$PWD
 export ACTIVE_ENV_DIR=/.e
 
 # INITIALIZATION FUNCTION FOR BASHRC SETUP
@@ -16,17 +16,17 @@ insertOnce() {
 
 createNewEnvironmentFile() {
     export ACTIVE_PROJECT_NAME=$1
-    export ACTIVE_PROJECT_PATH=$2
+    export ACTIVE_PROJECT_HOME=$2
     export ACTIVE_ENV_DIR=$3
-    export ACTIVE_ENV_PATH=$ACTIVE_PROJECT_PATH$ACTIVE_ENV_DIR
+    export ACTIVE_ENV_PATH=$ACTIVE_PROJECT_HOME$ACTIVE_ENV_DIR
     export ACTIVE_ENV_FILE_PATH=$ACTIVE_ENV_PATH/.e
     echo "#SPCOIN PROJECT CONFIGURATION FILE: $ACTIVE_PROJECT_NAME"                      | tee    $ACTIVE_ENV_FILE_PATH
     echo "#SPCOIN ENVIRONMENT CONFIGURATION SETUP SCRIPT: $ACTIVE_ENV_FILE_PATH"         | tee -a $ACTIVE_ENV_FILE_PATH
     echo "export ACTIVE_PROJECT_NAME=$ACTIVE_PROJECT_NAME"                               | tee -a $ACTIVE_ENV_FILE_PATH
-    echo "export ACTIVE_PROJECT_PATH=$ACTIVE_PROJECT_PATH"                               | tee -a $ACTIVE_ENV_FILE_PATH
+    echo "export ACTIVE_PROJECT_HOME=$ACTIVE_PROJECT_HOME"                               | tee -a $ACTIVE_ENV_FILE_PATH
     echo "export ACTIVE_ENV_PATH=$ACTIVE_ENV_PATH"                                       | tee -a $ACTIVE_ENV_FILE_PATH
-    echo "export ACTIVE_SCRIPTS_PATH=\$ACTIVE_PROJECT_PATH/scripts"                      | tee -a $ACTIVE_ENV_FILE_PATH
-    echo "export ACTIVE_LOGS_PATH=\$ACTIVE_PROJECT_PATH/logs"                            | tee -a $ACTIVE_ENV_FILE_PATH
+    echo "export ACTIVE_SCRIPTS_PATH=\$ACTIVE_PROJECT_HOME/scripts"                      | tee -a $ACTIVE_ENV_FILE_PATH
+    echo "export ACTIVE_LOGS_PATH=\$ACTIVE_PROJECT_HOME/logs"                            | tee -a $ACTIVE_ENV_FILE_PATH
 
 
 
@@ -38,15 +38,15 @@ createNewEnvironmentFile() {
 }
 
 #SET UP BASH ENVIRONMENT
-createNewEnvironmentFile $ACTIVE_PROJECT_NAME $ACTIVE_PROJECT_PATH $ACTIVE_ENV_DIR
+createNewEnvironmentFile $ACTIVE_PROJECT_NAME $ACTIVE_PROJECT_HOME $ACTIVE_ENV_DIR
 insertOnce "set -o vi" ~/.bashrc;
 
 echo "Adding sponsor coin startup configuration Files to bootstrap file ~/.bashrc"
 sed -i '/ACTIVE_ENV_FILE_PATH/d' ~/.bashrc
-sed -i '/ACTIVE_PROJECT_PATH/d' ~/.bashrc
+sed -i '/ACTIVE_PROJECT_HOME/d' ~/.bashrc
 echo "export ACTIVE_ENV_FILE_PATH=$ACTIVE_ENV_FILE_PATH" | tee -a ~/.bashrc;
 echo ". \$ACTIVE_ENV_FILE_PATH"                          | tee -a ~/.bashrc;
-echo "cd \$ACTIVE_PROJECT_PATH"                          | tee -a ~/.bashrc;
+echo "cd \$ACTIVE_PROJECT_HOME"                          | tee -a ~/.bashrc;
 
 echo "EXECUTING: . $ACTIVE_ENV_FILE_PATH"
 . $ACTIVE_ENV_FILE_PATH
