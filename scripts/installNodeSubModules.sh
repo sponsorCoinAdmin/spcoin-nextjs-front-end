6a3b62feaf8483ee17442385641c323b87371581
sponsorCoinAdmin <sponsorCoinAdmin@proton.me>
2025-01-16 03:27:58 -0500

    WIP

--- scripts/installNodeSubModules.sh
+++ scripts/installNodeSubModules.sh
@@ -1,38 +1,21 @@
 exeIfDirExists() {
-    local CURR_DIR=$PWD
     DIRECTORY=$1
     COMMAND=$2
-    echo "FROM ROOT DIR        : $PWD"
+    local CURR_DIR=$PWD
+    echo "CURRENT DIRECTORY : $PWD"
     if [ -d "$DIRECTORY" ]; then
-        echo "INSTALLING MODULES IN: $DIRECTORY"
-        echo "EXECUTING            : $COMMAND"
-        #  $COMMAND
+        echo INSTALLING in: SUB-DIRECTORY $DIRECTORY =====
+        cd $DIRECTORY
+        echo "EXECUTING: $COMMAND"
+        eval $COMMAND
     else 
-        echo "***ERROR: DIRECTORY $DIRECTORY not found"
+        echo "***ERROR: SUB-DIRECTORY $DIRECTORY NOT FOUND"
         echo "***ERROR: COMMAND $COMMAND NOT EXECUTED" 
     fi
     cd $CURR_DIR
 }
 
 clear
-echo "=======================spcoin-nextjs-front-end========================================="
+echo "======================= spcoin-nextjs-front-end ========================================="
 echo START INSTALLING NPM Modules in $PWD/node_modules-dev/spcoin-back-end
 exeIfDirExists ./node_modules-dev ". ./scripts/installNodeSubModules.sh"
-cd ..
-
-
-
-
-
-# echo START INSTALLING NPM Modules in $CURR_DIR/node_modules-dev/spcoin-back-end/spcoin-common/spcoin-lib
-# cd $CURR_DIR/node_modules-dev/spcoin-back-end/spcoin-common/spcoin-lib
-# npm i
-# cd ..
-# echo START INSTALLING NPM Modules in $CURR_DIR/node_modules-dev/spcoin-back-end/spcoin-access-modules
-# cd $CURR_DIR/node_modules-dev/spcoin-back-end/spcoin-access-modules
-# npm i
-# cd ..
-# echo START INSTALLING NPM Modules in $CURR_DIR/node_modules-dev/spcoin-back-end/spcoin-with-modules
-# cd $CURR_DIR/node_modules-dev/spcoin-back-end/spcoin-with-modules-cjs
-# npm i
-# cd ..
