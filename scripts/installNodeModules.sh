d4d334f3ca7f39e11b2b7e172f43322b5c21cfac
sponsorCoinAdmin <sponsorCoinAdmin@proton.me>
2025-01-22 18:25:21 -0500

    WIP

--- scripts/installNodeModules.sh
+++ scripts/installNodeModules.sh
@@ -1,111 +1,48 @@
-repeat(){
-    char=$1
-    length=$2
-    # echo length = $length
-    str=""
-    i=1
-    while [ $i -le $2 ]
+. ./scripts/findFileList.sh
+CURR_DIR=$PWD
+SEARCH_FILE="package.json"
+ExclusionList=(
+    "artifacts"
+    "node_modules"
+    "scripts"
+    "\.env"
+    "\.e"
+    "cache")
+ExclusionStr="${ExclusionList[@]}"
+
+installArrayModules() {
+    local arr=$@
+    echo =========================================== installNodeModules ===============================================
+    echo EXECUTION DIRECTORY: $CURR_DIR
+    doubleLine 110
+    for element in $arr
     do
-        # echo $i: $1
-        let "i++" 
-        str="$str$1"
-    done
-    echo $str
-}
-
-doubleLine() { 
-    $(repeat "=" $1) 
-}
-singleLine() {
-    $(repeat "-" $1)
-}
-underLine() {
-    $(repeat "_" $1)
-}
-
-doubleLine
-echo PDW=$PWD
-INSTALL_SCRIPT="/scripts/installNodeModules.sh"
-echo
-echo masterScript=$PWD/$INSTALL_SCRIPT
-masterScript=/c/Users/Robin/spCoin/JUNK2/SPCOIN-PROJECT-MODULES/scripts/installNodeModules.sh
-singleLine
-
-
-installChildrenModules() {
-    repeat "=" 80
-    pwd
-    indentSZ=$1
-    indentChar="."
-    indentSTR=$(repeat $indentChar $indentSZ)
-    echo "Indent = <$indentSTR>"
-    repeat "-" 80
-
-    ExclusionList=(
-    	"artifacts"
-    	"node_modules"
-    	"scripts"
-    	"\.env"
-    	"\.e"
-    	"cache")
-    ExclusionStr="${ExclusionList[@]}"
-    
-    # Print the Exclusion List String
-    echo "The list of Exclusion Directories: ${ExclusionStr}"
-    
-    clearDir() {
-        local dir=$1
-        dir1=${dir%*/}                 # remove the trailing "/"
-        echo $dir1 | sed "s|^\./||"    # remove the leading "./"
-    }
-    
-    indentEcho() {
-        echo $indentSTR$1
-    }
-    
-    processDir() {
-        local dir=$1
-	    INSTALL_SCRIPT="/scripts/installNodeModules.sh"
-        if [ -f  $dir/$INSTALL_SCRIPT ]; then
-            indentEcho "Installing SCRIPT MODULE $dir$INSTALL_SCRIPT"
-	    cd $dir
-        let indentSZ=$indentSZ+2
-	    . ./$INSTALL_SCRIPT $indentSZ
-        let indentSZ=$indentSZ-2
-	    cd ..
-        else
-            indentEcho "No Installation Script $dir$INSTALL_SCRIPT"
-        fi
-        #indentEcho "dir = ${dir}"  # print everything after the final "/"
-    }
-    
-    for dir in ./*/          # list directory names
-    do
-        dir=$(clearDir $dir)
-    
-    foundDir=$(echo $ExclusionStr | grep $dir)
-    
-    if [ "$foundDir" != "" ]; then
-        indentEcho "\*\*\*Excluding Directory: $dir"
-    else
-        #echo EXECUTING: processDir $dir
-        processDir $dir
-    fi
+         parentdir="$(dirname "$element")"
+	 echo EXECUTING: cd $parentdir
+         echo EXECUTING installNodeModules: npm -i $SEARCH_FILE
+         # npm -i $element
+	 echo EXECUTING: cd $CURR_DIR
+	 singleLine 110
     done
 }
 
 
-installLocalNodeCode() {
-    if [ -f "package.json" ]; then
-    echo 
-        indentEcho "Installing NPM Modules: $(pwd)"
-    else
-        indentEcho "Not an Node package directory"
-    fi
+installNodeModules() {
+    echo "installNodeModules():"
+
+    doubleLine 80
+    searchFile=$SEARCH_FILE
+    singleLine 80
+
+    getFilePathList $SEARCH_FILE $ExclusionStr
+    #myFileList=$(getFilePathList $searchFile)
+
+    sortedUniqueText=$(sortUniqueTextArray $scriptList)
+    echo sortedUniqueText=$sortedUniqueText
+    dumpArray $sortedUniqueText
+    echo
+    echo  sortedUniqueText=$sortedUniqueText
+    installArrayModules $sortedUniqueText
 }
 
-doubleLine
-installChildrenModules $1
-singleLine
-installLocalNodeCode
-underLine
+installNodeModules
