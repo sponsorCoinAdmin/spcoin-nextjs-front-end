da764c3775126f881c5c4804b70fd30e67617874
sponsorCoinAdmin <sponsorCoinAdmin@proton.me>
2025-03-06 11:10:45 -0500

    WIP

--- components/Buttons/PriceButton.tsx
+++ components/Buttons/PriceButton.tsx
@@ -1,8 +1,9 @@
 'use client';
+
 import React, { useEffect, useState } from 'react';
 import CustomConnectButton from './CustomConnectButton';
 import ExchangeButton from './ExchangeButton';
-import { exchangeContext } from "@/lib/context";
+import { useExchangeContext } from "@/lib/context/ExchangeContext";
 import DumpContextButton from './DumpContextButton';
 import { useAccount } from 'wagmi';
 import { Address } from 'viem';
@@ -10,37 +11,41 @@ import { ErrorMessage } from '@/lib/structure/types';
 
 type Props = {
     isLoadingPrice: boolean,
-    errorMessage:ErrorMessage|undefined,
-    setErrorMessage: (errorMessage:ErrorMessage|undefined) => void,
-    setResetAmounts: (resetAmounts:boolean) => void,
+    errorMessage: ErrorMessage | undefined,
+    setErrorMessage: (errorMessage: ErrorMessage | undefined) => void,
+    setResetAmounts: (resetAmounts: boolean) => void,
     toggleButton: boolean
-  }
+}
 
-const PriceButton = ({isLoadingPrice, errorMessage, setErrorMessage, setResetAmounts, toggleButton}:Props) => {    
-    const ACTIVE_ACCOUNT = useAccount()
-    const [ displayDumpContextButton, setDisplayDumpContextButton ] = useState<boolean>(exchangeContext.test.dumpContextButton)
-    const [ walletAccount, setWalletAccount ] = useState<Address|undefined>(undefined);
+const PriceButton = ({ isLoadingPrice, errorMessage, setErrorMessage, setResetAmounts, toggleButton }: Props) => {    
+    const { exchangeContext } = useExchangeContext();
+    const ACTIVE_ACCOUNT = useAccount();
+    
+    const [displayDumpContextButton, setDisplayDumpContextButton] = useState<boolean>(exchangeContext.test.dumpContextButton);
+    const [walletAccount, setWalletAccount] = useState<Address | undefined>(undefined);
 
     useEffect(() => {
-        setDisplayDumpContextButton(exchangeContext.test.dumpContextButton)
-    }, [exchangeContext.test.dumpContextButton])
+        setDisplayDumpContextButton(exchangeContext.test.dumpContextButton);
+    }, [exchangeContext.test.dumpContextButton]);
     
     useEffect(() => {
-        setWalletAccount(ACTIVE_ACCOUNT?.address)
+        setWalletAccount(ACTIVE_ACCOUNT?.address);
         // alert(`exchangeContext.test.dumpContextButton = ${exchangeContext.test.dumpContextButton}`)
-    }, [ACTIVE_ACCOUNT?.address])
+    }, [ACTIVE_ACCOUNT?.address]);
 
-    // setWalletAccount(ACTIVE_ACCOUNT?.address != undefined)
     return (
         <div>
-            {!walletAccount?
-                (<CustomConnectButton />) :
-                (<ExchangeButton isLoadingPrice={isLoadingPrice}
-                                 errorMessage={errorMessage}
-                                 setErrorMessage={setErrorMessage}
-                                 setResetAmounts={setResetAmounts}
-                                 toggleButton={toggleButton}/>)
-            }
+            {!walletAccount ? (
+                <CustomConnectButton />
+            ) : (
+                <ExchangeButton
+                    isLoadingPrice={isLoadingPrice}
+                    errorMessage={errorMessage}
+                    setErrorMessage={setErrorMessage}
+                    setResetAmounts={setResetAmounts}
+                    toggleButton={toggleButton}
+                />
+            )}
             {!displayDumpContextButton || <DumpContextButton />}
         </div>
     );
