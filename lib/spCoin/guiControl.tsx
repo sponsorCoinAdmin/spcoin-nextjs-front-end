bd380ee5b52d508d0a042ff0ba7c9fd85330da10
Robin <robin.e.melanson@gmail.com>
2025-05-15 16:40:24 -0400

    WIP

--- lib/spCoin/guiControl.tsx
+++ lib/spCoin/guiControl.tsx
@@ -23,16 +23,40 @@ const toggleElement = (element: any) => {
   }
 };
 
+const displaySpCoinContainers = (spCoinDisplay: SP_COIN_DISPLAY, exchangeContext: any) => {
+  switch (spCoinDisplay) {
+    case SP_COIN_DISPLAY.SELECT_RECIPIENT_BUTTON:
+      showElement("AddSponsorshipButton_ID");
+      hideElement("RecipientSelect_ID");
+      hideElement("SponsorRateConfig_ID");
+      exchangeContext.spCoinDisplay = spCoinDisplay;
+      break;
+    case SP_COIN_DISPLAY.SHOW_RECIPIENT_CONTAINER:
+      showElement("RecipientSelect_ID");
+      hideElement("AddSponsorshipButton_ID");
+      hideElement("SponsorRateConfig_ID");
+      exchangeContext.spCoinDisplay = spCoinDisplay;
+      break;
+    case SP_COIN_DISPLAY.SHOW_SPONSOR_RATE_CONFIG:
+      showElement("SponsorRateConfig_ID");
+      showElement("RecipientSelect_ID");
+      hideElement("AddSponsorshipButton_ID");
+      exchangeContext.spCoinDisplay = spCoinDisplay;
+      break;
+  }
+};
+
+
 export const spCoinStringDisplay = (spCoinDisplay: SP_COIN_DISPLAY | undefined): string => {
   switch (spCoinDisplay) {
     case SP_COIN_DISPLAY.OFF:
       return `spCoinDisplay(${SP_COIN_DISPLAY.OFF}) = OFF`;
     case SP_COIN_DISPLAY.SELECT_RECIPIENT_BUTTON:
       return `spCoinDisplay(${SP_COIN_DISPLAY.SELECT_RECIPIENT_BUTTON}) = SELECT_RECIPIENT_BUTTON`;
-    case SP_COIN_DISPLAY.MANAGE_RECIPIENT_BUTTON:
-      return `spCoinDisplay(${SP_COIN_DISPLAY.MANAGE_RECIPIENT_BUTTON}) = MANAGE_RECIPIENT_BUTTON`;
     case SP_COIN_DISPLAY.SHOW_RECIPIENT_CONTAINER:
       return `spCoinDisplay(${SP_COIN_DISPLAY.SHOW_RECIPIENT_CONTAINER}) = SHOW_RECIPIENT_CONTAINER`;
+    case SP_COIN_DISPLAY.MANAGE_RECIPIENT_BUTTON:
+      return `spCoinDisplay(${SP_COIN_DISPLAY.MANAGE_RECIPIENT_BUTTON}) = MANAGE_RECIPIENT_BUTTON`;
     case SP_COIN_DISPLAY.SHOW_SPONSOR_RATE_CONFIG:
       return `spCoinDisplay(${SP_COIN_DISPLAY.SHOW_SPONSOR_RATE_CONFIG}) = SHOW_SPONSOR_RATE_CONFIG`;
     default: return `spCoinDisplay(${String(spCoinDisplay)}) = ❓ UNKNOWN`;
