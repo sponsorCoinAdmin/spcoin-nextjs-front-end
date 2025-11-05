99879c962bf630e2bf066f4d63a7f14514c29867
Robin <robin.e.melanson@gmail.com>
2025-11-05 02:34:24 -0500

    WIP

--- components/Headers/TradeContainerHeader copy.tsx
+++ components/Headers/TradeContainerHeader copy.tsx
@@ -24,15 +24,16 @@ export default function TradeContainerHeader() {
 
   return (
     <div
-      id='TradeContainerHeader'
-      className='grid grid-cols-[auto_1fr_auto] items-center w-full box-border h-[50px] min-h-[50px] py-0 px-2.5 shrink-0 my-[3px]'
+      id="TradeContainerHeader"
+      className="grid grid-cols-[auto_1fr_auto] items-center w-full box-border h-[50px] min-h-[50px] py-0 px-2.5 shrink-0 my-[3px]"
     >
       <ConfigPanel showPanel={isConfigOpen} onClose={onCloseConfig as any} />
 
+      {/* Left */}
       <div
-        id='SponsorCoinLogo.png'
+        id="SponsorCoinLogo.png"
         onDoubleClick={() => exchangeContextDump(exchangeContext)}
-        className='flex items-center my-0'
+        className="flex items-center my-0"
       >
         {leftElement ?? (
           <ConnectButton
@@ -46,29 +47,31 @@ export default function TradeContainerHeader() {
         )}
       </div>
 
-      <h4 className='justify-self-center m-0 leading-none text-base font-semibold select-none text-center'>
+      {/* Center title */}
+      <h4 className="justify-self-center m-0 leading-none text-base font-semibold select-none text-center">
         {title}
       </h4>
 
-      <div className='flex items-center justify-end my-0'>
+      {/* Right */}
+      <div className="flex items-center justify-end my-0">
         {isTrading ? (
           <Image
             src={cog_png}
-            alt='Open settings'
-            title='Open settings'
+            alt="Open settings"
+            title="Open settings"
             onClick={onOpenConfig}
-            className='h-5 w-5 object-contain cursor-pointer transition duration-300 hover:rotate-[360deg]'
+            className="h-5 w-5 object-contain cursor-pointer transition duration-300 hover:rotate-[360deg]"
             priority
           />
         ) : (
           <CloseButton
             closeCallback={onClose}
-            className='
+            className="
               absolute top-2 right-[27px] h-10 w-10 rounded-full
               bg-[#243056] text-[#5981F3] flex items-center justify-center
               leading-none transition-colors text-3xl
               hover:bg-[#5981F3] hover:text-[#243056]
-            '
+            "
           />
         )}
       </div>
