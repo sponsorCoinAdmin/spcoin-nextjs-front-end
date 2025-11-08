8e820b435a7d9192a6e592b610cca6c2279e4668
Robin <robin.e.melanson@gmail.com>
2025-11-08 03:47:12 -0500

    WIP

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,7 @@ module.exports = {
     `./components/**/*.{js,ts,jsx,tsx,mdx}`,
     `./lib/**/*.{js,ts,jsx,tsx,mdx}`,
     `./pages/**/*.{js,ts,jsx,tsx,mdx}`,
-    `./styles/**/*.{css}`,
+    `./styles/**/*.css`,
   ],
 
   darkMode: `class`,
