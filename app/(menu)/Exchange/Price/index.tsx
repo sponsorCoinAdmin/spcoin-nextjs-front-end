2f71c55592bfde5da84b531d8ea0efd0c4b6b61b
Robin <robin.e.melanson@gmail.com>
2025-11-07 23:49:17 -0500

    WIP

--- app/(menu)/Exchange/Price/index.tsx
+++ app/(menu)/Exchange/Price/index.tsx
@@ -1,24 +1,14 @@
 // File: app/(menu)/Exchange/Price/index.tsx
-
 'use client';
 
 import styles from '@/styles/Exchange.module.css';
-
 import { MainTradingPanel } from '@/components/views';
 
-// Removed: useDisplayStateCorrection (redundant after sanitize + enum coercion)
 import { usePriceErrorEffect } from '@/lib/hooks/usePriceErrorEffect';
 import { useResetAmountsOnTokenChange } from '@/lib/hooks/useResetAmountsOnTokenChange';
 
-// import { createDebugLogger } from '@/lib/utils/debugLogger';
-
-// const LOG_TIME = false;
-// const DEBUG_ENABLED = process.env.NEXT_PUBLIC_DEBUG_LOG_PRICE_VIEW === 'true';
-// const debugLog = createDebugLogger('PriceView', DEBUG_ENABLED, LOG_TIME);
-
 export default function PriceView() {
-
-  // keep side-effects that matter for UX
+  // UX-relevant side effects
   usePriceErrorEffect();
   useResetAmountsOnTokenChange();
 
--- app/(menu)/Exchange/index.tsx
+++ app/(menu)/Exchange/index.tsx
@@ -1,30 +1,12 @@
 // File: app/(menu)/Exchange/index.tsx
 
-import PriceView from "./Price/index";
-import { EXCHANGE_STATE } from "@/lib/structure";
 import React from 'react';
+import PriceView from './Price';
 
-let setExchangeState: (value: EXCHANGE_STATE) => void;
-
-export function Home() {
+export default function Page() {
   return (
     <main className={`flex min-h-screen flex-col items-center justify-between p-24`}>
       <PriceView />
     </main>
   );
-}
-
-const Index = () => {
-  return (
-    <div>
-      <Home /> {/* ✅ Render as JSX, not as a direct function call */}
-    </div>
-  );
-}
-
-export default Index;
-
-export {
-  setExchangeState,
-  EXCHANGE_STATE
 }
