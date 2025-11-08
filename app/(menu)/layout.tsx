8c9cf490816541f273c75dfa91fd9140e2c3e2bc
Robin <robin.e.melanson@gmail.com>
2025-11-08 02:15:23 -0500

    WIP

--- app/(menu)/layout.tsx
+++ app/(menu)/layout.tsx
@@ -1,5 +1,4 @@
 // File: app/(menu)/layout.tsx
-
 import type { Metadata } from 'next'
 import '@/app/globals.css'
 import Header from '@/components/panes/header'
@@ -10,21 +9,10 @@ export const metadata: Metadata = {
   description: 'A decentralized cryptocurrency exchange platform powered by spCoin.',
 }
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
-  // keep the param to satisfy Next.js layout typing, but don't render it
-  void children
-
+export default function RootLayout() {
   return (
-    <>
-      <SpCoinProviders>
-        <Header />
-        {/* {children} */}
-        {/* <Footer /> */}
-      </SpCoinProviders>
-    </>
+    <SpCoinProviders>
+      <Header />
+    </SpCoinProviders>
   )
 }
