9cdab365b76dc5553f50f3445a4f37849cbf83bd
Robin <robin.e.melanson@gmail.com>
2025-11-11 17:07:16 -0500

    WIP

--- app/(menu)/Exchange/page.tsx
+++ app/(menu)/Exchange/page.tsx
@@ -1,31 +1,9 @@
 // File: app/(menu)/Exchange/page.tsx
 'use client'
 
-import React, { useEffect } from 'react'
+import React from 'react'
 import Component from './index'
 
 export default function ExchangePage() {
-  useEffect(() => {
-    let cleanup: (() => void) | undefined
-
-    ;(async () => {
-      try {
-        const mod = await import('@/lib/debug/panels/installDocClickProbe')
-        if (typeof mod.installDocClickProbe === 'function') {
-          const ret = mod.installDocClickProbe()
-          if (typeof ret === 'function') cleanup = ret
-        }
-      } catch {
-        // Probe is optional; ignore if it doesn't exist in this build
-      }
-    })()
-
-    return () => {
-      if (typeof cleanup === 'function') {
-        try { cleanup() } catch {}
-      }
-    }
-  }, [])
-
   return <Component />
 }
