e2a5366c684355ab70e80c543dc45537c8316440
Robin <robin.e.melanson@gmail.com>
2026-04-15 18:47:25 -0400

    WIP

--- app/(menu)/(dynamic)/SponsorCoinLab/jsonMethods/spCoin/read/defs/getMasterAccountElement.ts
+++
@@ -1,9 +0,0 @@
-// File: app/(menu)/(dynamic)/SponsorCoinLab/methods/spcoin/read/defs/getMasterAccountElement.ts
-import type { MethodDef } from '../../../shared/types';
-
-export const methodDef: MethodDef = {
-  title: 'getMasterAccountElement',
-  params: [{ label: 'Index', placeholder: 'uint256', type: 'uint' }],
-};
-
-export default methodDef;
--- app/(menu)/(dynamic)/SponsorCoinLab/jsonMethods/spCoin/read/defs/getMasterAccountList.ts
+++
@@ -1,6 +0,0 @@
-// File: app/(menu)/(dynamic)/SponsorCoinLab/methods/spcoin/read/defs/getMasterAccountList.ts
-import type { MethodDef } from '../../../shared/types';
-
-export const methodDef: MethodDef = { title: 'getMasterAccountList', params: [] };
-
-export default methodDef;
